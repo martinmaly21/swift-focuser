00e56c4f9d568f04c74b22b923e1544c58eab90a
Martin Maly <40171261+martinmaly21@users.noreply.github.com>
2022-04-18 15:33:32 -0400

    Support TextEditor

--- Sources/Focuser/TextEditorIntrospect.swift
+++ Sources/Focuser/TextEditorIntrospect.swift
@@ -6,22 +6,75 @@
 //
 
 import SwiftUI
+import Introspect
+
+class TextEditorObserver: NSObject, UITextViewDelegate {
+    var onReturnTap: () -> () = {}
+    var onDismiss: () -> () = {}
+    weak var forwardToDelegate: UITextViewDelegate?
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        forwardToDelegate?.textViewShouldBeginEditing?(textView) ?? true
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        forwardToDelegate?.textViewDidBeginEditing?(textView) 
+    }
+
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        return forwardToDelegate?.textViewShouldEndEditing?(textView) ?? true
+    }
+
+    func textViewDidEndEditing(_ textView: UITextView) {
+        onDismiss()
+        forwardToDelegate?.textViewDidEndEditing?(textView)
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        forwardToDelegate?.textView?(textView, shouldChangeTextIn: range, replacementText: text) ?? true
+    }
+
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        forwardToDelegate?.textViewDidChangeSelection?(textView)
+    }
+}
 
 @available(iOS 13.0, *)
 public struct FocusModifierTextEditor<Value: FocusStateCompliant & Hashable>: ViewModifier {
     @Binding var focusedField: Value?
     var equals: Value
-    @State var observer = TextFieldObserver()
+    @State var observer = TextEditorObserver()
     
     public func body(content: Content) -> some View {
         content
-            .introspectTextView { tv in
+            .introspectTextView { textView in
+                if !(textView.delegate is TextEditorObserver) {
+                    observer.forwardToDelegate = textView.delegate
+                    textView.delegate = observer
+                }
+                
+                /// when user taps return we navigate to next responder
+                observer.onReturnTap = {
+                    focusedField = focusedField?.next ?? Value.last
+                }
+                
+                observer.onDismiss = {
+                     focusedField = nil
+                }
+                
+                /// to show kayboard with `next` or `return`
+                if equals.hashValue == Value.last.hashValue {
+                    textView.returnKeyType = .done
+                } else {
+                    textView.returnKeyType = .next
+                }
+                
                 if focusedField == equals {
-                    tv.becomeFirstResponder()
+                    textView.becomeFirstResponder()
                 }
             }
             .simultaneousGesture(TapGesture().onEnded {
-              focusedField = equals
+                focusedField = equals
             })
     }
 }
