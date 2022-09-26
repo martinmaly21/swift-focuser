521297635a0c92b65b00a4a31edc3102db47f308
Martin Maly <martinmaly66@hotmail.com>
2022-09-26 14:55:46 -0400

    Alternative

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["Focuser"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/martinmaly21/SwiftUI-Introspect.git", .branch("master"))
+        .package(url: "https://github.com/martinmaly21/SwiftUI-Introspect.git", .revision("1f68e763c16965b28a9c51d35036e365174b3085"))
     ],
     targets: [
         .target(
